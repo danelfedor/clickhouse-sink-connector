bf16c4f4137a946b4d9c144813929ac38a1877e9
wenqi.hu <wenqi.hu@tianyancapital.com>
2025-07-31 13:42:54 +0800

    fix bug in debezium

--- sink-connector-lightweight/build_docker.sh
+++ sink-connector-lightweight/build_docker.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-mvn clean install -DskipTests=true
+mvn clean install -DskipTests=true -Djavac.args="-g"
 today_date=$(date +%F)
 
-docker login registry.gitlab.com
 docker build . -t clickhouse_debezium_embedded:${today_date} --no-cache
-docker tag clickhouse_debezium_embedded:${today_date} registry.gitlab.com/altinity-public/container-images/clickhouse_debezium_embedded:${today_date}
-docker push registry.gitlab.com/altinity-public/container-images/clickhouse_debezium_embedded:${today_date}
+docker tag clickhouse_debezium_embedded:${today_date} pilchard/clickhouse_debezium_embedded:${today_date}
+docker push pilchard/clickhouse_debezium_embedded:${today_date}
