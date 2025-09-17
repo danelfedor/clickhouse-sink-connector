a69f4e6e59d1de48cbb1036aa27b0086225dfb8f
Wenqi Hu <wenqi.hu@tianyancapital.com>
2025-09-17 15:06:19 +0800

    fix test

--- sink-connector/src/test/java/com/altinity/clickhouse/sink/connector/db/DbWriterTest.java
+++ sink-connector/src/test/java/com/altinity/clickhouse/sink/connector/db/DbWriterTest.java
@@ -1,4 +1,4 @@
-package com.altinity.clickhouse.sink.connector.db;
+package java.com.altinity.clickhouse.sink.connector.db;
 
 import com.altinity.clickhouse.sink.connector.ClickHouseSinkConnectorConfig;
 import com.altinity.clickhouse.sink.connector.converters.ClickHouseConverter;
@@ -148,19 +148,19 @@ public class DbWriterTest {
                 BaseDbWriter.SYSTEM_DB, new ClickHouseSinkConnectorConfig(new HashMap<>()));
         DbWriter writer = new DbWriter(dbHostName, port, database, tableName, userName, password,
                 new ClickHouseSinkConnectorConfig(new HashMap<>()), null, conn);
-        MutablePair<DBMetadata.TABLE_ENGINE, String> result = new DBMetadata().getTableEngineUsingShowTable(writer.getConnection(), "test", "employees");
+        MutablePair<DBMetadata.TABLE_ENGINE, String> result = new DBMetadata().getTableEngineUsingSystemTables(writer.getConnection(), "test", "employees");
         Assert.assertTrue(result.getLeft() == DBMetadata.TABLE_ENGINE.REPLACING_MERGE_TREE);
         Assert.assertTrue(result.getRight().equalsIgnoreCase("_version22"));
 
-        MutablePair<DBMetadata.TABLE_ENGINE, String> result_test = new DBMetadata().getTableEngineUsingShowTable(writer.getConnection(), "test", "employees");
+        MutablePair<DBMetadata.TABLE_ENGINE, String> result_test = new DBMetadata().getTableEngineUsingSystemTables(writer.getConnection(), "test", "employees");
         Assert.assertTrue(result_test.getLeft() == DBMetadata.TABLE_ENGINE.REPLACING_MERGE_TREE);
         Assert.assertTrue(result_test.getRight().equalsIgnoreCase("_version22"));
 
-        MutablePair<DBMetadata.TABLE_ENGINE, String> result_employees = new DBMetadata().getTableEngineUsingShowTable(writer.getConnection(), "employees", "employees");
+        MutablePair<DBMetadata.TABLE_ENGINE, String> result_employees = new DBMetadata().getTableEngineUsingSystemTables(writer.getConnection(), "employees", "employees");
         Assert.assertTrue(result_employees.getLeft() == DBMetadata.TABLE_ENGINE.REPLACING_MERGE_TREE);
         Assert.assertTrue(result_employees.getRight().equalsIgnoreCase("_version_employees"));
 
-        MutablePair<DBMetadata.TABLE_ENGINE, String> resultProducts = new DBMetadata().getTableEngineUsingShowTable(writer.getConnection(), "default", "products");
+        MutablePair<DBMetadata.TABLE_ENGINE, String> resultProducts = new DBMetadata().getTableEngineUsingSystemTables(writer.getConnection(), "default", "products");
         Assert.assertTrue(resultProducts.getLeft() == DBMetadata.TABLE_ENGINE.COLLAPSING_MERGE_TREE);
         Assert.assertTrue(resultProducts.getRight().equalsIgnoreCase("sign"));
     }
