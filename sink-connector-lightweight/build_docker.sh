5d8ba2c97ed0ef85036c71d60e205a10628aa3a7
wenqi.hu <wenqi.hu@tianyancapital.com>
2026-08-20 08:03:27 +0000

    change build docker

--- sink-connector-lightweight/build_docker.sh
+++ sink-connector-lightweight/build_docker.sh
@@ -4,5 +4,5 @@ mvn clean install -DskipTests=true -Djavac.args="-g"
 today_date=$(date +%F)
 
 docker build . -t clickhouse_debezium_embedded:${today_date} --no-cache
-docker tag clickhouse_debezium_embedded:${today_date} pilchard/clickhouse_debezium_embedded:${today_date}-1
-docker push pilchard/clickhouse_debezium_embedded:${today_date}-1
+docker tag clickhouse_debezium_embedded:${today_date} harbor.ivocap.com/qti/clickhouse_debezium_embedded:${today_date}
+docker push harbor.ivocap.com/qti/clickhouse_debezium_embedded:${today_date}
